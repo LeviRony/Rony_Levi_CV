19d9801520b5b0289844203ba88936b77a7124b0
RonyBoom <ronylevi99@gmail.com>
2025-04-03 11:23:48 +0300

    remove files

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,9 @@ const nextConfig = {
   output: 'export',
   distDir: 'out',
   images: {
-<<<<<<< HEAD
     unoptimized: true, // Fix for Next.js images on GitHub Pages
   },
   assetPrefix: isProd ? "https://github.com/LeviRony/Rony_Levi_CV" : "", // Ensure correct URL format
-=======
-    unoptimized: true, 
-  },
-  assetPrefix: isProd ? "https://github.com/LeviRony/Rony_Levi_CV" : "", 
->>>>>>> main
   basePath: isProd ? "/Rony_Levi_CV" : "",
 };
 
